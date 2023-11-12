f42f71b63b00dce9c4cbc40d384cf06dd420b640
geoscriber <100984391+geoscriber@users.noreply.github.com>
2023-11-12 22:54:30 +0800

    init

---
+++ data.js
@@ -0,0 +1,181 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-image-1",
+      "name": "Image 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.652062662702857,
+        "pitch": 1.5707963267948966,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.109271829811836,
+          "pitch": 0.8110115686580492,
+          "rotation": 0,
+          "target": "2-image-3"
+        },
+        {
+          "yaw": -2.43420886230572,
+          "pitch": 0.8857032244496992,
+          "rotation": 0,
+          "target": "3-image-4"
+        },
+        {
+          "yaw": -2.9143126762932674,
+          "pitch": 0.8570441816373329,
+          "rotation": 0,
+          "target": "1-image-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-image-2",
+      "name": "Image 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7478704659837527,
+          "pitch": 1.356203730934082,
+          "rotation": 0,
+          "target": "2-image-3"
+        },
+        {
+          "yaw": -1.1066329538079849,
+          "pitch": 0.9124210265668502,
+          "rotation": 0,
+          "target": "3-image-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-image-3",
+      "name": "Image 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.16207627142649983,
+        "pitch": 0.5194807853887955,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6385352348096127,
+          "pitch": 0.18091655097834547,
+          "rotation": 0,
+          "target": "3-image-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-image-4",
+      "name": "Image 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.41416940186213935,
+        "pitch": 0.31027713702261117,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2056285523408725,
+          "pitch": 0.07257656498786957,
+          "rotation": 0,
+          "target": "2-image-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Tatalon 1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
